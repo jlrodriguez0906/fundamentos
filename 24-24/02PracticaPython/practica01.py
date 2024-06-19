33f2f73791356ab7bd493213af2e01b2e6b2fed0
JOSE RODRIGUEZ <jlrodriguez0906@gmail.com>
2024-06-18 19:48:39 -0500

    [ADD] Inicio de programas en Python

---
+++ 24-24/02PracticaPython/practica01.py
@@ -0,0 +1,13 @@
+# Programa de saludo
+nombre = "JOSE LUIS"
+print("HOLA ", nombre)
+
+# Programa de suma
+print ("++++++++++++++++++++++++++++++++")
+print ("SUMA DE NUMEROS")
+print ("Ingrese numero 1:")
+numero1 = input()
+print ("Ingrese numero 2:")
+numero2 = int( input() )
+x = int(numero1) + numero2
+print ("La suma es: ", x)
