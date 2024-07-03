0c7095c7ceccd8a867627dca0750313fc513f5e1
JOSE RODRIGUEZ <jlrodriguez0906@gmail.com>
2024-07-02 19:30:37 -0500

    [ADD] Inicio de ordenar números

---
+++ 24-24/03PracticaPython/ejemplo04.py
@@ -0,0 +1,24 @@
+print("ORDENAR 3 NÚMEROS")
+
+print("Ingrese numero 1: ")
+num1 = int(input())
+
+print("Ingrese numero 2: ")
+num2 = int(input())
+
+print("Ingrese numero 3: ")
+num3 = int(input())
+
+if num1 > num2 > num3:
+    print(num3, " - ", num2, " - ", num1)
+elif num1 > num3 > num2:
+    print(num2, " - ", num3, " - ", num1)
+elif num2 > num1 > num3:
+    print(num3, " - ", num1, " - ", num2)
+elif num2 > num3 > num1:
+    print(num1, " - ", num3, " - ", num2)
+elif num3 > num1 > num2:
+    print(num2, " - ", num1, " - ", num3)
+else:
+    # num3 > num2 > num1:
+    print(num1, " - ", num2, " - ", num3)
