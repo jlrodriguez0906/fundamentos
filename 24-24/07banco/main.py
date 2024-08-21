1a1417b9e3fb331768760869991f15c420573a9a
JOSE RODRIGUEZ <jlrodriguez0906@gmail.com>
2024-08-20 19:45:00 -0500

    [ADD] Creación de MENÚ

--- 24-24/07banco/main.py
+++ 24-24/07banco/main.py
@@ -1,11 +1,40 @@
-from modelo.cuenta import Cuenta
+# from modelo.cuenta import Cuenta
 
-cue = Cuenta("José Rodríguez","100",20)
-# cue.tipo_cuenta = "CORRIENTE"
+# cue = Cuenta("José Rodríguez","100",20)
+# # cue.tipo_cuenta = "CORRIENTE"
 
-print( cue.informacion_cuenta() )
-print( cue.depositar(85) )
-print( cue.depositar(35) )
-print( cue.informacion_cuenta() )
-print( cue.retirar(50) )
-print( cue.informacion_cuenta() )
+# print( cue.informacion_cuenta() )
+# print( cue.depositar(85) )
+# print( cue.depositar(35) )
+# print( cue.informacion_cuenta() )
+# print( cue.retirar(50) )
+# print( cue.informacion_cuenta() )
+
+# print("---------------------")
+
+# tx = cue.transaccion
+# num = 1
+# for item in tx:
+#     print( "%s. %s"%(num,item) )
+#     num = num + 1
+
+import os
+
+cuentas = []
+
+while True:
+    os.system("cls")
+    os.system("dir")
+    print("BIENVENIDO - BANCO FLORES")
+    print("=========================")
+    print("1. Cuenta nueva")
+    print("2. Depósito")
+    print("3. Retiro")
+    print("4. Consulta de saldo")
+    print("5. Consulta de transacciones")
+    print("6. Salir")
+    print("")
+    print("Ingrese opción: ")
+    op = input()
+    if op == "6":
+        break
--- 24-24/07banco/modelo/cuenta.py
+++ 24-24/07banco/modelo/cuenta.py
@@ -5,12 +5,14 @@ class Cuenta:
     nro_cuenta = ""
     tipo_cuenta = "AHORRO"
     saldo = 0.00
+    transaccion = []
 
     # Métodos - Operaciones
     def __init__(self, cli, nro, saldo):
         self.cliente = cli
         self.nro_cuenta = nro
         self.saldo = saldo
+        self.transaccion.append("DEPOSITO: %s\tSALDO: %s"%(saldo,saldo) )
 
     def informacion_cuenta(self):
         return "C. %s %s - CLIENTE: %s - SALDO: $%s"%(self.tipo_cuenta,
@@ -19,10 +21,12 @@ class Cuenta:
     
     def depositar(self, monto):
         self.saldo = self.saldo + monto
+        self.transaccion.append("DEPOSITO: %s\tSALDO: %s "%(monto, self.saldo) )
         return "Depósito realizado con éxito ($%s)"%(monto)
 
     def retirar(self, monto):
         if monto <= self.saldo:
             self.saldo = self.saldo - monto
+            self.transaccion.append("RETIRO: -%s\tSALDO: %s "%(monto, self.saldo) )
             return "Retiro realizado con éxito (- $%s)"%(monto)
         return "Saldo insuficiente"
